310578e8deea5d142fb10d07d88cfadf1d9f3c07
William <byweimar@gmail.com>
2023-12-13 20:51:31 -0500

    finished

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,51 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here 
+fetch('/products', {
+    method: 'POST',
+    body: 'name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
 
 
 
-/* ============================== Phase 2 ============================== */
+    /* ============================== Phase 2 ============================== */
 
-// Your code here 
+    .then(response => {
+        console.log('Status Code: ', response.statusCode);
+        console.log('Content-Type Header: ', response.headers.get('Content-Type'));
+        console.log('URL of Response: ', response.url);
+    }
+
+
+    )
 
 
 
 /* ============================== Phase 3 ============================== */
 
-// Your code here 
+const bodyData = new URLSearchParams({
+    name: "Caribbean Delight Coffee",
+    description: "Made by Manatee Coffee",
+    price: 11.99,
+    categories: "grocery"
+});
+
+fetch('/products', {
+    method: 'POST',
+    body: bodyData,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+
+    .then(response => {
+        console.log('Status Code: ', response.statusCode);
+        console.log('Content-Type Header: ', response.headers.get('Content-Type'));
+        console.log('URL of Response: ', response.url);
+    }
+
+
+    );
